c4961345e06364146348264263f567c93e0bb98b
zhaozhuang <zzzhaozzzz@163.com>
2021-10-21 17:14:46 +0800

    fix: 默认

--- src/sdk.d.ts
+++ src/sdk.d.ts
@@ -107,17 +107,17 @@ interface MenuShareApp extends AppMessageShare {
     /** 分享类型,music、video或link，不填默认为link */ type: "";
     /** 如果type是music或video，则要提供数据链接，默认为空 */ dataUrl: "";
 }
-interface MenuShareQQ extends AppMessageShare {}
-interface MenuShareWeibo extends AppMessageShare {}
-interface MenuShareQZone extends AppMessageShare {}
+interface MenuShareQQ extends AppMessageShare { }
+interface MenuShareWeibo extends AppMessageShare { }
+interface MenuShareQZone extends AppMessageShare { }
 interface ChooseImage extends Extra {
-    /** 默认9 */ count: number;
-    /** 可以指定是原图还是压缩图，默认二者都有*/ sizeType: Array<
-        "original" | "compressed"
-    >;
-    /** 可以指定来源是相册还是相机，默认二者都有 */ sourceType: Array<
-        "album" | "camera"
-    >;
+    /** 默认9 */ count?: number;
+    /** 可以指定是原图还是压缩图，默认二者都有*/ sizeType?: Array<
+    "original" | "compressed"
+>;
+    /** 可以指定来源是相册还是相机，默认二者都有 */ sourceType?: Array<
+    "album" | "camera"
+>;
 }
 
 interface PreviewImage {
@@ -159,34 +159,34 @@ interface Location {
     /** 经度，浮点数，范围为180 ~ -180。*/ longitude: number;
     /** 位置名*/ name: string;
     /** 地址详情说明*/ address: string;
-    /** 地图缩放级别,整型值,范围从1~28。默认为最大*/ scale: number;
+    /** 地图缩放级别,整型值,范围从1~28。默认为最大*/ scale?: number;
     /** 在查看位置界面底部显示的超链接,可点击跳转*/ infoUrl: string;
 }
 interface GetLocation extends Extra {
-    /** 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'*/ type: string;
+    /** 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'*/ type?: string;
 }
 interface SearchBeacon {
     /** 摇周边的业务ticket, 系统自动添加在摇出来的页面链接后面*/ ticket: string;
 }
 interface HideMenuItem {
     /** 要隐藏的菜单项，只能隐藏“传播类”和“保护类”按钮*/ menuList: Array<
-        MenuItemShare | MenuItemGuard
-    >;
+    MenuItemShare | MenuItemGuard
+>;
 }
 interface ShowMenuItem {
     /** 要显示的菜单项*/ menuList: Array<
-        MenuItemBase | MenuItemShare | MenuItemGuard
-    >;
+    MenuItemBase | MenuItemShare | MenuItemGuard
+>;
 }
 interface ScanQRCode extends Extra {
-    /** 默认为0，扫描结果由微信处理，1则直接返回扫描结果*/ needResult: 0 | 1;
-    /** 可以指定扫二维码还是一维码，默认二者都有*/ scanType: Array<
-        "qrCode" | "barCode"
-    >;
+    /** 默认为0，扫描结果由微信处理，1则直接返回扫描结果*/ needResult?: 0 | 1;
+    /** 可以指定扫二维码还是一维码，默认二者都有*/ scanType?: Array<
+    "qrCode" | "barCode"
+>;
 }
 interface ProductSpecificView {
     /** 商品id*/ productId: string;
-    /** 0.默认值，普通商品详情页1.扫一扫商品详情页2.小店商品详情页*/ viewType: string;
+    /** 0.默认值，普通商品详情页1.扫一扫商品详情页2.小店商品详情页*/ viewType?: string;
 }
 interface Card extends Extra {
     /** 门店Id*/ shopId: string;
@@ -194,51 +194,51 @@ interface Card extends Extra {
     /** 卡券Id*/ cardId: string;
     /** 卡券签名时间戳*/ timestamp: number;
     /** 卡券签名随机串*/ nonceStr: string;
-    /** 签名方式，默认'SHA1'*/ signType: string;
+    /** 签名方式，默认'SHA1'*/ signType?: string;
     /** 卡券签名*/ cardSign: string;
 }
 interface AddCard extends Extra {
     /** 卡券列表*/ cardList: Array<{
-        cardId: string;
-        /**
-         * 卡券列表
-         * cardExt本身是一个JSON字符串，是商户为该张卡券分配的唯一性信息，包含以下字段
-         *
-         * 指定的卡券code码，只能被领一次。自定义code模式的卡券必须填写，非自定义code和预存code模式的卡券不必填写。详情见： 是否自定义code码
-         * (参与签名,可选)
-         * code?;
-         *
-         * 指定领取者的openid，只有该用户能领取。bind_openid字段为true的卡券必须填写，bind_openid字段为false不必填写。
-         * (参与签名,可选)
-         *openid?;
-         *
-         * 时间戳，商户生成从1970年1月1日00:00:00至今的秒数,即当前的时间,且最终需要转换为字符串形式;由商户生成后传入,不同添加请求的时间戳须动态生成，若重复将会导致领取失败！。
-         * (参与签名,必选)
-         *timestamp;
-         *
-         * 随机字符串，由开发者设置传入， 加强安全性（若不填写可能被重放请求） 。随机字符串，不长于32位。推荐使用大小写字母和数字，不同添加请求的nonce须动态生成，若重复将会导致领取失败。
-         * (参与签名,可选)
-         *nonce_str?;
-         *
-         * 卡券在第三方系统的实际领取时间，为东八区时间戳（UTC+8,精确到秒）。当卡券的有效期类型为 DAT E_TYPE_FIX_TERM时专用，标识卡券的实际生效时间，用于解决商户系统内起始时间和领取时间不同步的问题。
-         * (可选)
-         * fixed_begintimestamp?;
-         *
-         * 领取渠道参数，用于标识本次领取的渠道值。
-         * (可选)
-         * outer_str?;
-         *
-         * 签名，商户将接口列表中的参数按照指定方式进行签名,签名方式使用SHA1。
-         * 具体签名方案参见 https://developers.weixin.qq.com/doc/offiaccount/OA_Web_Apps/JS-SDK.html;
-         * 由商户按照规范签名后传入。
-         * (必选)
-         * signature;
-         *
-         */
-        cardExt: string;
-    }>;
+    cardId: string;
+    /**
+     * 卡券列表
+     * cardExt本身是一个JSON字符串，是商户为该张卡券分配的唯一性信息，包含以下字段
+     *
+     * 指定的卡券code码，只能被领一次。自定义code模式的卡券必须填写，非自定义code和预存code模式的卡券不必填写。详情见： 是否自定义code码
+     * (参与签名,可选)
+     * code?;
+     *
+     * 指定领取者的openid，只有该用户能领取。bind_openid字段为true的卡券必须填写，bind_openid字段为false不必填写。
+     * (参与签名,可选)
+     *openid?;
+     *
+     * 时间戳，商户生成从1970年1月1日00:00:00至今的秒数,即当前的时间,且最终需要转换为字符串形式;由商户生成后传入,不同添加请求的时间戳须动态生成，若重复将会导致领取失败！。
+     * (参与签名,必选)
+     *timestamp;
+     *
+     * 随机字符串，由开发者设置传入， 加强安全性（若不填写可能被重放请求） 。随机字符串，不长于32位。推荐使用大小写字母和数字，不同添加请求的nonce须动态生成，若重复将会导致领取失败。
+     * (参与签名,可选)
+     *nonce_str?;
+     *
+     * 卡券在第三方系统的实际领取时间，为东八区时间戳（UTC+8,精确到秒）。当卡券的有效期类型为 DAT E_TYPE_FIX_TERM时专用，标识卡券的实际生效时间，用于解决商户系统内起始时间和领取时间不同步的问题。
+     * (可选)
+     * fixed_begintimestamp?;
+     *
+     * 领取渠道参数，用于标识本次领取的渠道值。
+     * (可选)
+     * outer_str?;
+     *
+     * 签名，商户将接口列表中的参数按照指定方式进行签名,签名方式使用SHA1。
+     * 具体签名方案参见 https://developers.weixin.qq.com/doc/offiaccount/OA_Web_Apps/JS-SDK.html;
+     * 由商户按照规范签名后传入。
+     * (必选)
+     * signature;
+     *
+     */
+    cardExt: string;
+}>;
 }
-interface OpenCard extends Omit<AddCard, keyof Extra> {}
+interface OpenCard extends Omit<AddCard, keyof Extra> { }
 interface WXPay extends Extra {
     /** 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符*/ timestamp: number;
     /** 支付签名随机串，不长于 32 位*/ nonceStr: string;
